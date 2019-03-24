1343e297ba54ec64bcd35d274f3aa0ec2640670f
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-03-24 19:56:20 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,9 +14,9 @@ def third_element (array)
   array[3]
 end 
 
-def last_element (array)
-  array.pop
-end
+#def last_element (array)
+  #array.pop
+#end
 
-def first_element_with_array_methods(array)
-  array.first
+#def first_element_with_array_methods(array)
+  #array.first
